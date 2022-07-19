6126cddd60d3613bfbafe79f2583a50f7c370e47
nuno.martins <nmartins@redhat.com>
2022-07-19 11:05:42 +0200

    Created Terraform content

---
+++ images/ansible/setup-scripts/terraform-ansible/setup_script.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+# Terraform Lab Setup Script
+
+# Configure gitea and repo for builds
+ansible-playbook gitea_setup.yml -e @track_vars.yml -i inventory.ini
+
+# Configure controller environment 
