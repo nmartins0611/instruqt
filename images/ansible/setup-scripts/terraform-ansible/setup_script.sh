e41939912a91fd6a389e4c736ae1feba9884c80e
nuno.martins <nmartins@redhat.com>
2022-07-25 12:59:23 +0200

    Created Terraform content

--- images/ansible/setup-scripts/terraform-ansible/setup_script.sh
+++ images/ansible/setup-scripts/terraform-ansible/setup_script.sh
@@ -11,15 +11,15 @@ ansible-playbook /tmp/setup-scripts/terraform-ansible/controller_setup.yml -e @/
 
 # Configure vs-code content
 # Setup manual content
-mv /tmp/setup-scripts/terraform-ansible/main.tf /home/rhel/lab_exercises/1.Terraform_Basics
-mv /tmp/setup-scripts/terraform-ansible/main.j2 /home/rhel/lab_exercises/2.Terraform_Ansible
-mv /tmp/setup-scripts/terraform-ansible/variables.j2 /home/rhel/lab_exercises/2.Terraform_Ansible
+# mv /tmp/setup-scripts/terraform-ansible/main.tf /home/rhel/lab_exercises/1.Terraform_Basics
+# mv /tmp/setup-scripts/terraform-ansible/main.j2 /home/rhel/lab_exercises/2.Terraform_Ansible
+# mv /tmp/setup-scripts/terraform-ansible/variables.j2 /home/rhel/lab_exercises/2.Terraform_Ansible
 
-yum install -y unzip
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+# yum install -y unzip
+# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+# unzip awscliv2.zip
+# sudo ./aws/install
 
-sudo chmod -R 777 /home/rhel/lab_exercises
+# sudo chmod -R 777 /home/rhel/lab_exercises
 
-aws ec2 create-default-vpc
+# aws ec2 create-default-vpc
