0009dbdfd8526277ec789209774c7c6b1050cb42
nuno.martins <nmartins@redhat.com>
2022-08-05 13:01:59 +0200

    Created Terraform content

--- images/ansible/setup-scripts/terraform-ansible/setup_script.sh
+++ images/ansible/setup-scripts/terraform-ansible/setup_script.sh
@@ -7,7 +7,7 @@ sudo yum -y install terraform
 ansible-playbook /tmp/setup-scripts/terraform-ansible/gitea_setup.yml -e @/tmp/setup-scripts/terraform-ansible/track_vars.yml -i /tmp/setup-scripts/terraform-ansible/inventory.ini
 
 # Configure Rrepo for builds
-ansible-playbook /tmp/setup-scripts/terraform-ansible/gitea.yml -e @/tmp/setup-scripts/terraform-ansible/track_vars.yml -i /tmp/setup-scripts/terraform-ansible/inventory.ini
+ansible-playbook /tmp/setup-scripts/terraform-ansible/gitea_build.yml -e @/tmp/setup-scripts/terraform-ansible/track_vars.yml -i /tmp/setup-scripts/terraform-ansible/inventory.ini
 
 # Configure controller environment 
 ansible-playbook /tmp/setup-scripts/terraform-ansible/controller_setup.yml -e @/tmp/setup-scripts/terraform-ansible/track_vars.yml -i /tmp/setup-scripts/terraform-ansible/inventory.ini
