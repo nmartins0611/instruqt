bfcdbb588b904521d9a100449951e546c12e0924
nuno.martins <nmartins@redhat.com>
2022-08-15 11:47:02 +0200

    Created Terraform content

--- images/ansible/setup-scripts/terraform-ansible/aws_setup.sh
+++ images/ansible/setup-scripts/terraform-ansible/aws_setup.sh
@@ -5,3 +5,6 @@ export AWS_DEFAULT_REGION=us-east-1
 
 # Create default vpc
 aws ec2 create-default-vpc >> /tmp/aws_vpc
+
+# Create keypair from controller
+aws ec2 import-key-pair --key-name "controller_ssh" --public-key-material fileb://~/.ssh/id_rsa.pub
