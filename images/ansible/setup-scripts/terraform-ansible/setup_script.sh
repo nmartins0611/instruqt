c9c96def1c62df3d4d3f3d563e24ec358b181e3b
nuno.martins <nmartins@redhat.com>
2022-08-29 23:04:54 +0200

    Created Terraform content

--- images/ansible/setup-scripts/terraform-ansible/setup_script.sh
+++ images/ansible/setup-scripts/terraform-ansible/setup_script.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 # Terraform Lab Setup Script
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-#sudo yum -y install terraform
+sudo yum -y install terraform
 
 # Configure gitea and repo for builds
 ansible-playbook /tmp/setup-scripts/terraform-ansible/gitea_setup.yml -e @/tmp/setup-scripts/terraform-ansible/track_vars.yml -i /tmp/setup-scripts/terraform-ansible/inventory.ini
