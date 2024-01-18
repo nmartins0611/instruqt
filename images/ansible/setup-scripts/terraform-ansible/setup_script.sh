8c53c86986d51c94cc94adc19feca34c7d3b977d
nmartins0611 <nmartins@redhat.com>
2024-01-18 20:02:34 +0200

    Update setup_script.sh

--- images/ansible/setup-scripts/terraform-ansible/setup_script.sh
+++ images/ansible/setup-scripts/terraform-ansible/setup_script.sh
@@ -6,10 +6,10 @@ sudo yum -y localinstall  /tmp/terraform-ansible/images/ansible/setup-scripts/te
 /tmp/terraform-ansible/images/ansible/setup-scripts/terraform-ansible/terraform-1.2.8-1.x86_64.rpm
 
 # Configure gitea and repo for builds
-ansible-playbook /tmp/terraform-ansible/images/ansible/setup-scripts/terraform-ansible/gitea_setup.yml -e @/tmp/terraform-ansible/images/ansible/setup-scripts/terraform-ansible/track_vars.yml -i /tmp/setup-scripts/terraform-ansible/inventory.ini
+ansible-playbook /tmp/terraform-ansible/images/ansible/setup-scripts/terraform-ansible/gitea_setup.yml -e @/tmp/terraform-ansible/images/ansible/setup-scripts/terraform-ansible/track_vars.yml -i /tmp/terraform-ansible/images/ansible/setup-scripts/terraform-ansible/inventory.ini
 
 # Configure Repo for builds
-ansible-playbook /tmp/terraform-ansible/images/ansible/setup-scripts/terraform-ansible/gitea_build.yml -e @/tmp/setup-scripts/terraform-ansible/track_vars.yml -i /tmp/terraform-ansible/images/ansible/setup-scripts/terraform-ansible/inventory.ini
+ansible-playbook /tmp/terraform-ansible/images/ansible/setup-scripts/terraform-ansible/gitea_build.yml -e @/tmp/terraform-ansible/images/ansible/setup-scripts/terraform-ansible/track_vars.yml -i /tmp/terraform-ansible/images/ansible/setup-scripts/terraform-ansible/inventory.ini
 
 # Configure controller environment 
-ansible-playbook /tmp/terraform-ansible/images/ansible/setup-scripts/terraform-ansible/controller_setup.yml -e @/tmp/setup-scripts/terraform-ansible/track_vars.yml -i /tmp/terraform-ansible/images/ansible/setup-scripts/terraform-ansible/inventory.ini
+ansible-playbook /tmp/terraform-ansible/images/ansible/setup-scripts/terraform-ansible/controller_setup.yml -e @/tmp/terraform-ansible/images/ansible/setup-scripts/terraform-ansible/track_vars.yml -i /tmp/terraform-ansible/images/ansible/setup-scripts/terraform-ansible/inventory.ini
