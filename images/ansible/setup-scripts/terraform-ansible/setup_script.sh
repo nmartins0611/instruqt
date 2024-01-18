c7654e5ffc2a9bdf96d12e4fbb041f10fbf244e4
nmartins0611 <nmartins@redhat.com>
2024-01-18 21:41:48 +0200

    Update setup_script.sh

--- images/ansible/setup-scripts/terraform-ansible/setup_script.sh
+++ images/ansible/setup-scripts/terraform-ansible/setup_script.sh
@@ -3,8 +3,6 @@
 #sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y localinstall  /tmp/terraform-ansible/images/ansible/setup-scripts/terraform-ansible/terraform-1.2.8-1.x86_64.rpm
 
-/tmp/terraform-ansible/images/ansible/setup-scripts/terraform-ansible/terraform-1.2.8-1.x86_64.rpm
-
 # Configure gitea and repo for builds
 ansible-playbook /tmp/terraform-ansible/images/ansible/setup-scripts/terraform-ansible/gitea_setup.yml -e @/tmp/terraform-ansible/images/ansible/setup-scripts/terraform-ansible/track_vars.yml -i /tmp/terraform-ansible/images/ansible/setup-scripts/terraform-ansible/inventory.ini
 
